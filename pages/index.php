f4e7488e909e43e71364c054c4c10fc9879cb4f9
gericlois <gericlois.casugay@lorma.edu>
2024-07-25 17:58:23 +0800

    Added CTA BM

--- pages/index.php
+++ pages/index.php
@@ -39,134 +39,155 @@
 
         </section><!-- /Hero Section -->
 
-        <!-- Featured Services Section -->
-        <section id="featured-services" class="featured-services section">
 
-            <!-- About Section -->
-            <section id="about" class="about section">
+        <!-- Alt Pricing Section -->
+        <section id="alt-pricing" class="alt-pricing section">
 
-                <div class="container">
+            <div class="container">
 
-                    <div class="row gy-4">
+                <div class="row gy-4 pricing-item featured mt-4" data-aos="fade-up" data-aos-delay="200">
+                    <div class="col-lg-3 d-flex align-items-center justify-content-center">
+                        <h3>Become a member</h3>
+                    </div>
+                    <div class="col-lg-6 d-flex align-items-center justify-content-center">
+                        <ul>
+                            <li><i class="bi bi-check"></i> <span>Find fellow fans of your favorite teams! Create an account now
+                                    and join the millions of sports enthusiasts around the world!</span></li>
+                        </ul>
+                    </div>
+                    <div class="col-lg-3 d-flex align-items-center justify-content-center">
+                        <div class="text-center"><a href="#" class="buy-btn">Join Now!</a></div>
+                    </div>
+                </div><!-- End Pricing Item -->
 
-                        <div class="col-lg-4 position-relative align-self-start order-lg-last order-first" data-aos="fade-up" data-aos-delay="200">
-                            <img src="../assets/img/about.jpg" class="img-fluid" alt="">
-                            <a href="https://www.youtube.com/watch?v=vB-dUGMFV90" class="glightbox pulsating-play-btn"></a>
-                        </div>
+            </div>
 
-                        <div class="col-lg-8 content order-last  order-lg-first" data-aos="fade-up" data-aos-delay="100">
-                            <h3>What Is SportsTXT?</h3>
-                            <p>
-                                Being a sports fan is not just about watching games. SportsTXT is an online platform designed to deliver the ultimate sports fan experience.
-                                Engage with other fans, get insider facts about your favorite teams, and so much more.
-                            </p>
-                            <br>
-                            <h3>Our Mission</h3>
-                            <p>
-                                Our mission is to provide our users with a seamless and enjoyable online experience, offering a diverse range of high-quality content and services that inform, inspire, and engage. Through innovative design and intuitive functionality, we aim to create a platform that connects people and facilitates meaningful interactions.
-                            </p>
-                        </div>
+        </section><!-- /Alt Pricing Section -->
 
+        <!-- About Section -->
+        <section id="about" class="about section">
+
+            <div class="container">
+
+                <div class="row gy-4">
+                    <div class="col-lg-4 position-relative align-self-start order-lg-last order-first" data-aos="fade-up" data-aos-delay="200">
+                        <img src="../assets/img/about.jpg" class="img-fluid" alt="">
+                        <a href="https://www.youtube.com/watch?v=vB-dUGMFV90" class="glightbox pulsating-play-btn"></a>
+                    </div>
+
+                    <div class="col-lg-8 content order-last  order-lg-first" data-aos="fade-up" data-aos-delay="100">
+                        <h3>What Is SportsTXT?</h3>
+                        <p>
+                            Being a sports fan is not just about watching games. SportsTXT is an online platform designed to deliver the ultimate sports fan experience.
+                            Engage with other fans, get insider facts about your favorite teams, and so much more.
+                        </p>
+                        <br>
+                        <h3>Our Mission</h3>
+                        <p>
+                            Our mission is to provide our users with a seamless and enjoyable online experience, offering a diverse range of high-quality content and services that inform, inspire, and engage. Through innovative design and intuitive functionality, we aim to create a platform that connects people and facilitates meaningful interactions.
+                        </p>
                     </div>
 
                 </div>
 
-            </section><!-- /About Section -->
+            </div>
+
+        </section><!-- /About Section -->
 
 
-            <!-- How it Works Section -->
-            <section id="how" class="how section">
+        <!-- How it Works Section -->
+        <section id="how" class="how section">
 
-                <!-- Section Title -->
-                <div class="container section-title" data-aos="fade-up">
-                    <span>How It Works<br></span>
-                    <h2>How It Works</h2>
-                    <p><b>SportsTXT is a fully customizable community platform that connects directly with members and fans in real-time giving them a true sense of belonging.</b><br>
-                        It provides sports clubs and teams with a unique opportunity to create more personalized experiences and open up new revenue streams. Youll be able to capture valuable data and tailor content to specific member segments.</p>
-                </div><!-- End Section Title -->
+            <!-- Section Title -->
+            <div class="container section-title" data-aos="fade-up">
+                <span>How It Works<br></span>
+                <h2>How It Works</h2>
+                <p><b>SportsTXT is a fully customizable community platform that connects directly with members and fans in real-time giving them a true sense of belonging.</b><br>
+                    It provides sports clubs and teams with a unique opportunity to create more personalized experiences and open up new revenue streams. Youll be able to capture valuable data and tailor content to specific member segments.</p>
+            </div><!-- End Section Title -->
 
-            </section><!-- /How it Works Section -->
+        </section><!-- /How it Works Section -->
 
-            <!-- Platform Section -->
-            <section id="platform" class="platform section">
+        <!-- Platform Section -->
+        <section id="platform" class="platform section">
 
-                <!-- Section Title -->
-                <div class="container section-title" data-aos="fade-up">
-                    <span>The Platform of Firsts</span>
-                    <h2>The Platform of Firsts</h2>
-                </div><!-- End Section Title -->
+            <!-- Section Title -->
+            <div class="container section-title" data-aos="fade-up">
+                <span>The Platform of Firsts</span>
+                <h2>The Platform of Firsts</h2>
+            </div><!-- End Section Title -->
 
-                <div class="container">
+            <div class="container">
 
-                    <div class="row gy-4 align-items-center features-item">
-                        <div class="col-md-4 d-flex align-items-center" data-aos="zoom-out" data-aos-delay="100">
-                            <img src="../assets/img/platform-1.png" class="img-fluid" alt="">
-                        </div>
-                        <div class="col-md-8" data-aos="fade-up" data-aos-delay="100">
-                            <h3>Members First</h3>
-                            <p>It's time to elevate your fan engagement beyond just using social media and SportsTXT brings your members straight into the heart of your club through exclusive stories, new experiences, games and unique offers.
-                            </p>
-                        </div>
-                    </div><!-- Platform Item -->
+                <div class="row gy-4 align-items-center features-item">
+                    <div class="col-md-4 d-flex align-items-center" data-aos="zoom-out" data-aos-delay="100">
+                        <img src="../assets/img/platform-1.png" class="img-fluid" alt="">
+                    </div>
+                    <div class="col-md-8" data-aos="fade-up" data-aos-delay="100">
+                        <h3>Members First</h3>
+                        <p>It's time to elevate your fan engagement beyond just using social media and SportsTXT brings your members straight into the heart of your club through exclusive stories, new experiences, games and unique offers.
+                        </p>
+                    </div>
+                </div><!-- Platform Item -->
 
-                    <div class="row gy-4 align-items-center features-item">
-                        <div class="col-md-4 d-flex align-items-center" data-aos="zoom-out" data-aos-delay="100">
-                            <img src="../assets/img/platform-2.png" class="img-fluid" alt="">
-                        </div>
-                        <div class="col-md-8" data-aos="fade-up" data-aos-delay="100">
-                            <h3>Safety First</h3>
-                            <p>
+                <div class="row gy-4 align-items-center features-item">
+                    <div class="col-md-4 d-flex align-items-center" data-aos="zoom-out" data-aos-delay="100">
+                        <img src="../assets/img/platform-2.png" class="img-fluid" alt="">
+                    </div>
+                    <div class="col-md-8" data-aos="fade-up" data-aos-delay="100">
+                        <h3>Safety First</h3>
+                        <p>
                             SportsTXT offers 24/7 protection for sports clubs with real-time monitoring and content analysis powered by a combination of human expertise and AI technology. With SportsTXT, clubs can be confident in the quality and safety of their reputation and all user-generated content.
-                            </p>
-                        </div>
-                    </div><!-- Platform Item -->
+                        </p>
+                    </div>
+                </div><!-- Platform Item -->
 
-                    <div class="row gy-4 align-items-center features-item">
-                        <div class="col-md-4 d-flex align-items-center" data-aos="zoom-out" data-aos-delay="100">
-                            <img src="../assets/img/platform-3.png" class="img-fluid" alt="">
-                        </div>
-                        <div class="col-md-8" data-aos="fade-up" data-aos-delay="100">
-                            <h3>Data First</h3>
-                            <p>
+                <div class="row gy-4 align-items-center features-item">
+                    <div class="col-md-4 d-flex align-items-center" data-aos="zoom-out" data-aos-delay="100">
+                        <img src="../assets/img/platform-3.png" class="img-fluid" alt="">
+                    </div>
+                    <div class="col-md-8" data-aos="fade-up" data-aos-delay="100">
+                        <h3>Data First</h3>
+                        <p>
                             Contrary to social media, you have full ownership of your data. Your personal dashboard collects and analyzes important insights about members' usage of the platform, demographics and other data to help you make better business decisions.
-                            </p>
-                        </div>
-                    </div><!-- Platform Item -->
+                        </p>
+                    </div>
+                </div><!-- Platform Item -->
 
-                    <div class="row gy-4 align-items-center features-item">
-                        <div class="col-md-4 d-flex align-items-center" data-aos="zoom-out" data-aos-delay="100">
-                            <img src="../assets/img/platform-4.png" class="img-fluid" alt="">
-                        </div>
-                        <div class="col-md-8" data-aos="fade-up" data-aos-delay="100">
-                            <h3>Value First</h3>
-                            <p>
+                <div class="row gy-4 align-items-center features-item">
+                    <div class="col-md-4 d-flex align-items-center" data-aos="zoom-out" data-aos-delay="100">
+                        <img src="../assets/img/platform-4.png" class="img-fluid" alt="">
+                    </div>
+                    <div class="col-md-8" data-aos="fade-up" data-aos-delay="100">
+                        <h3>Value First</h3>
+                        <p>
                             By making a few small tweaks in the way you present your content, you will be amazed at how it can significantly boost your profits. You have the opportunity to set your content prices based on your own judgment and enhance its worth with the support of sponsors.
-                            </p>
-                        </div>
-                    </div><!-- Platform Item -->                 
-                    
-                </div>
+                        </p>
+                    </div>
+                </div><!-- Platform Item -->
 
-            </section><!-- /Platform Section -->
+            </div>
 
-            <!-- Get in Touch Section -->
-            <section id="call-to-action" class="call-to-action section dark-background">
+        </section><!-- /Platform Section -->
 
-                <img src="../assets/img/cta-bgs.jpg" alt="">
+        <!-- Get in Touch Section -->
+        <section id="call-to-action" class="call-to-action section dark-background">
 
-                <div class="container">
-                    <div class="row justify-content-center" data-aos="zoom-in" data-aos-delay="100">
-                        <div class="col-xl-10">
-                            <div class="text-center">
-                                <h3>Get in touch</h3>
-                                <p>Build A Unified Community with SportsTXT.</p>
-                                <a class="cta-btn" href="#">Contact Us</a>
-                            </div>
+            <img src="../assets/img/cta-bgs.jpg" alt="">
+
+            <div class="container">
+                <div class="row justify-content-center" data-aos="zoom-in" data-aos-delay="100">
+                    <div class="col-xl-10">
+                        <div class="text-center">
+                            <h3>Get in touch</h3>
+                            <p>Build A Unified Community with SportsTXT.</p>
+                            <a class="cta-btn" href="#">Contact Us</a>
                         </div>
                     </div>
                 </div>
+            </div>
 
-            </section><!-- /Get in Touch Section -->
+        </section><!-- /Get in Touch Section -->
 
     </main>
 
